878bff88256a549eb45c8f6f9e9291bd758c817a
Leonardo <leonardo.rodriguez92@gmail.com>
2023-05-30 10:27:15 -0400

    mount order, cart and review routes

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,19 @@ const authRoutes = require('./auth');
 const userRoutes = require('./users');
 const productRoutes = require('./products');
 const categoryRoutes = require('./categories');
-// const cartRoutes = require('./cart');
-// const orderRoutes = require('./orders');
+const cartRoutes = require('./cart');
+const orderRoutes = require('./orders');
+const reviewRoutes = require('./reviews');
 // const wishlistRoutes = require('./wishlist');
-// const reviewRoutes = require('./reviews');
 
 // Mount the routes
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/products', productRoutes);
 router.use('/categories', categoryRoutes);
-// router.use('/cart', cartRoutes);
-// router.use('/orders', orderRoutes);
+router.use('/cart', cartRoutes);
+router.use('/orders', orderRoutes);
+router.use('/reviews', reviewRoutes);
 // router.use('/wishlist', wishlistRoutes);
-// router.use('/reviews', reviewRoutes);
 
 module.exports = router;
